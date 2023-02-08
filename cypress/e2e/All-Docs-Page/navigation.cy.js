89e71b55092f4c96e840fe71b818745c65895f75
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-08 16:35:31 -0500

    developed(navigation):developed test codes to verify navigation

---
+++ cypress/e2e/All-Docs-Page/navigation.cy.js
@@ -0,0 +1,37 @@
+// Test Case 1: Verify that the page is loaded properly and the URL is correct.
+// Test Case 2: Verify that the page has all the necessary elements, including header, footer, categories, etc.
+// Test Case 3: Verify the responsive design of the page on different screen sizes, including mobile, tablet, and desktop. (Mobile, Visual)
+
+describe("Verify Navigation", () => {
+  it("the page is loaded properly and the URL is correct", () => {
+    cy.visit("https://www.tvo.org/series-docs/browse/categories/all");
+    cy.url().should("include", "/series-docs/browse/categories/all");
+  });
+
+  it("the page has all the necessary elements, including header, footer, categories menu bar", () => {
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+    cy.get(".categories").should("be.visible");
+  });
+
+  it("Verify the responsive design of the page on different screen sizes, including mobile, tablet, and desktop", () => {
+    // verifies that the page is responsive on mobile, tablet, and desktop and still shows the header, footer, and categories menu bar
+    cy.viewport("iphone-6");
+    cy.wait(1000);
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+    cy.get(".categories").should("be.visible");
+
+    cy.viewport(1024, 768);
+    cy.wait(1000);
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+    cy.get(".categories").should("be.visible");
+
+    cy.viewport(1920, 1080);
+    cy.wait(1000);
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+    cy.get(".categories").should("be.visible");
+  });
+});
