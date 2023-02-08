aa315f427737dc1686a08973b118f5718f0415d7
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-08 18:00:57 -0500

    developed(videoPlayback):developed test codes to verify video playback

--- cypress/e2e/All-Docs-Page/videoPlayback.cy.js
+++ cypress/e2e/All-Docs-Page/videoPlayback.cy.js
@@ -4,7 +4,38 @@
 // Test Case 3: Verify that the user can pause, play, and stop the video.
 
 describe("Verify Video Playback", () => {
-  it("user can play a video by clicking on it", () => {});
-  it("video starts playing and the controls are displayed", () => {});
-  it("user can pause, play, and stop the video", () => {});
+  it("user can play a video by clicking on it", () => {
+    cy.visit("https://www.tvo.org/series-docs/browse/categories/all");
+    cy.get(".document-card").first().click();
+    cy.get(".play-button").click();
+    cy.get("video").should("be.visible");
+  });
+
+  it("video starts playing and the controls are displayed", () => {
+    cy.get(".video-play").should("be.visible");
+    cy.get(".video-play").click();
+    cy.get(".video-pause").should("be.visible");
+    cy.get(".skip-back").should("be.visible");
+    cy.get(".skip-ahead").should("be.visible");
+    cy.get(".volume").should("be.visible");
+    cy.get(".fullscreen").should("be.visible");
+    cy.get("video-pause").click();
+  });
+
+  it("user can pause, play, and stop the video", () => {
+    cy.get(".video-play")
+      .click()
+      .then(($video) => {
+        $video[0].play();
+      });
+    // once the video starts playing, check props
+    cy.get("video")
+      .should("have.prop", "paused", false)
+      .and("have.prop", "ended", false);
+    // pause the video
+    cy.get(".video-pause").click();
+    cy.get("video")
+      .should("have.prop", "paused", true)
+      .and("have.prop", "ended", false);
+  });
 });
