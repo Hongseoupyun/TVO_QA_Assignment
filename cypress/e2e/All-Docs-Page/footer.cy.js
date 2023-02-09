0d6c4dd1eecc48dd002cde0df5ce3b660c989da7
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-08 20:43:46 -0500

    developed(footer):developed test codes to verify footer

--- cypress/e2e/All-Docs-Page/footer.cy.js
+++ cypress/e2e/All-Docs-Page/footer.cy.js
@@ -5,8 +5,26 @@
 //Test Case 4: Verify that the social media icons are functional and redirect to the correct social media page.
 
 describe("Verify Footer", () => {
-  it("the footer is present and visible", () => {});
-  it("the footer contains all the necessary elements, such as links to important pages and social media icons", () => {});
-  it("the links in the footer redirect to the correct page", () => {});
-  it("the social media icons are functional and redirect to the correct social media page.", () => {});
+  it("the footer is present and visible", () => {
+    cy.visit("https://www.tvo.org/series-docs/browse/categories/all");
+    cy.get("#footer").should("be.visible");
+  });
+  it("the footer contains all the necessary elements, such as links to important pages and social media icons", () => {
+    cy.get("#footer .links").should("be.visible");
+    cy.get("#footer .social-media").should("be.visible");
+  });
+  it("the links in the footer redirect to the correct page", () => {
+    cy.get("#footer .links a").each(($el) => {
+      const href = $el.prop("href");
+      cy.wrap($el).click();
+      cy.url().should("eq", href);
+    });
+  });
+  it("the social media icons are functional and redirect to the correct social media page", () => {
+    cy.get("#footer .social-media a").each(($el) => {
+      const href = $el.prop("href");
+      cy.wrap($el).click();
+      cy.url().should("eq", href);
+    });
+  });
 });
