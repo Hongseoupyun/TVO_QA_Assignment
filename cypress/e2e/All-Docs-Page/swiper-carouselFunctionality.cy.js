50b19f4d883aa0662f56afda820a3921b6b65763
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-09 09:16:51 -0500

    developed(swiper-carouselFunctionality):developed test codes to verify carousel-swiper functionality

--- cypress/e2e/All-Docs-Page/swiper-carouselFunctionality.cy.js
+++ cypress/e2e/All-Docs-Page/swiper-carouselFunctionality.cy.js
@@ -4,7 +4,38 @@
 //Test Case 3: Verify that the user can click on the left and right arrows to navigate through the carousel.
 
 describe("Verify Swiper-carousel Functionality", () => {
-  it("the swiper carousel is present and visible", () => {});
-  it("the swiper carousel displays multiple slides", () => {});
-  it("user can click on the left and right arrows to navigate through the carousel", () => {});
+  it("the swiper carousel is present and visible", () => {
+    cy.visit("https://www.tvo.org/series-docs/browse/categories/all");
+    cy.get(".carousel-nav").should("be.visible");
+    cy.get(".swiper-slide").should("be.visible");
+  });
+
+  it("the swiper carousel displays multiple slides", () => {
+    cy.get(".swiper-slide").should("have.length.greaterThan", 1);
+    cy.get(".carousel-nav").should("have.length.greaterThan", 1);
+  });
+  
+  it("user can click on the left and right arrows to navigate through the carousel", () => {
+    cy.get(".carousel-button-next").click();
+    cy.get(".carousel-nav-active").should(
+      "not.have.class",
+      "swiper-slide-prev"
+    );
+    cy.get(".carousel-button-prev").click();
+    cy.get(".carousel-nav-active").should(
+      "not.have.class",
+      "swiper-slide-next"
+    );
+
+    cy.get(".swiper-button-next").click();
+    cy.get(".swiper-slide-active").should(
+      "not.have.class",
+      "swiper-slide-prev"
+    );
+    cy.get(".swiper-button-prev").click();
+    cy.get(".swiper-slide-active").should(
+      "not.have.class",
+      "swiper-slide-next"
+    );
+  });
 });
