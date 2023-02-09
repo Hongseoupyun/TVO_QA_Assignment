ea669b63b0e12fcebb023d29865650b824441f68
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-08 20:03:15 -0500

    developed(filterCategories):developed test codes to verify filter categories functionality

--- cypress/e2e/All-Docs-Page/filterCategories.cy.js
+++ cypress/e2e/All-Docs-Page/filterCategories.cy.js
@@ -4,7 +4,29 @@
 // Test Case 3: Verify that the user is able to select different categories from the menu bar
 
 describe("Verify Filter Categories", () => {
-  it("user is able to select different categories from the dropdown list and selected category is highlighted in menu bar and the correct content is displayed", () => {});
+  it("user is able to select different categories from the dropdown list and correct content is displayed and highlights in menu bar", () => {
+    cy.visit("https://www.tvo.org/series-docs/browse/categories/all");
+    cy.get(".Docs & Series")
+      .trigger("mouseover")
+      .then(() => {
+        cy.get(".Docs & Series .dropdown").should("be.visible");
+        cy.get(".Docs & Series .dropdown a").each(($el) => {
+          const text = $el.text();
+          cy.wrap($el).click();
+          cy.url().should("not.include", "/series-docs/browse/categories/all");
+          cy.get(".category h1").contains(text);
+          cy.get(".category-nav a.selected").should("have.text", text);
+        });
+      });
+  });
 
-  it("user is able to select different categories from the menu bar and selected category is highlighted in menu bar and the correct content is displayed", () => {});
+  it("user is able to select different categories from the menu bar and correct content is displayed and highlights in menu bar", () => {
+    cy.get(".category-nav a").each(($el) => {
+      const text = $el.text();
+      cy.wrap($el).click();
+      cy.url().should("not.include", "/series-docs/browse/categories/all");
+      cy.get(".category h1").contains(text);
+      cy.get(".category-nav a.selected").should("have.text", text);
+    });
+  });
 });
